118c0072aa512dbe64b307cba9fa1aac6e224c03
Bahar Sarac <bbhrsrc@gmail.com>
2026-04-22 02:35:19 +0300

    The project was created, the file structure was modified, the .gitignore content was edited, and access to localhost was granted

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-slate-100">
+      <h1 className="text-6xl font-black text-cyan-400 drop-shadow-md">
+        WellLoop
+      </h1>
+      <p className="mt-4 text-xl text-slate-100">Installation complete! 🎉</p>
+    </div>
+  )
+}
+
+export default App
